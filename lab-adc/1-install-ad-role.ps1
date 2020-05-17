1ceaafc2ae6c2733b949e1b58dcba2e8c34e38bf
Aaron <aaroneg@users.noreply.github.com>
2020-05-17 00:20:01 -0500

    Update 1-install-ad-role.ps1

--- lab-adc/1-install-ad-role.ps1
+++ lab-adc/1-install-ad-role.ps1
@@ -2,4 +2,4 @@
 $SafeModeAdminPassword='P@ssw0rd2'
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 # Install the domain with a very bad password for the safe-mode administrator
-Install-ADDSForest -DomainName 'ad.lab.test -SafeModeAdministratorPassword ($SafeModeAdminPassword|ConvertTo-SecureString -AsPlainText -Force) -installDNS -Force
+Install-ADDSForest -DomainName 'ad.lab.test' -SafeModeAdministratorPassword ($SafeModeAdminPassword|ConvertTo-SecureString -AsPlainText -Force) -installDNS -Force
